34571e49df55177939bae7cf4e20f1742e17ded0
darmasup86 <achdarma.biker@gmail.com>
2026-06-16 12:01:34 +0700

    First commit - Project Taskflow

---
+++ resources/views/about.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+	
+	<title>About Us</title>
+</head>
+<body>
+	
+    <center>
+	<h1>About Us</h1>
+
+	<p><strong>Nama Lengkap:</strong> [Achmad Darma Supriatno, A.Md.Kom]</p>
+
+	<p><strong>Usia:</strong> [31 Tahun]</p>
+	<p><strong>No.hp:</strong> [081299337122]</p>
+<figure>
+    
+    <img src="{{ asset('images/bg.jpeg') }}" alt="Foto Profil" width="300" />
+    <figcaption>Foto Profil</figcaption>
+</figure>
+
+
+	<p><strong>Kesibukan Saat Ini:</strong> [bekerja sehari-hari]</p>
+
+	<p><strong>Alasan Mengikuti Bootcamp Laravel:</strong> [ingin membuat aplikasi handal untuk kepegawaian di tempat kerja berbasis laravel 13]</p>
+</center>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AboutController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/about', [AboutController::class, 'index']);
